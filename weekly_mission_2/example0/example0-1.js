e7ec5eb29233d6116fb6e323599421312cd15e5e
Mike&rade <hoodloom07@outlook.es>
2022-04-13 21:39:03 -0500

    Add example 0-3

---
+++ weekly_mission_2/example0/example0-1.js
@@ -0,0 +1,8 @@
+console.log ("Example 0 weekly two")
+
+// Creacion de objetos
+const myFirstObject = {
+
+}
+
+console.log("Mi objeto esta vacio ahora: " + myFirstObject);
---
+++ weekly_mission_2/example0/example0-3.js
@@ -0,0 +1,7 @@
+console.log("Function Example")
+
+const names = ["Carlo", "Miguel", "Sarai", "Pablo", "Zoe"];
+
+console.log(names);
+
+//Minuto del live 13.04.22: 1:35:00
