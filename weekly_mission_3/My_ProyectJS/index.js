edc7f51dc4c591329b1c80ae5016df86929e2fd2
Mike&rade <hoodloom07@outlook.es>
2022-04-19 02:20:24 -0500

    modified package.jsoo with start: node

---
+++ weekly_mission_3/My_ProyectJS/index.js
@@ -0,0 +1,10 @@
+const MissionCommander = require('./app/missionCommander');
+
+const carlo = new MissionCommander("Carlo");
+console.log(carlo.name);
+//MODULAR es hacer diferentes scripts para no tener todo en una solo.
+
+
+/*En el archivo package.json podemos agrear un start: node index.js para poder inicializar
+    con el coomando npm start
+*/    
