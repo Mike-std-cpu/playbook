3cb9681204d23ebfb6f1ace63468665bd3b0347f
Mike&rade <hoodloom07@outlook.es>
2022-04-21 19:30:30 -0500

    Practica 3 terminada, creacion de setters class User

--- weekly_mission_3/twitter/app/models/user.js
+++ weekly_mission_3/twitter/app/models/user.js
@@ -1,11 +1,43 @@
 class User {
-    constructor(id,userName, name, bio, dataCreated, lastUpdate){
+    constructor(id,userName, name, bio){
         this._id = id;
         this._userName = userName;
         this._name = name;
         this._bio = bio;
-        this._dataCreated = dataCreated;
-        this._lastUpdate = lastUpdate;
+        this._dataCreated = new Date(); // new Date te da la hora del SO, es decir la hora actual
+        this._lastUpdate = new Date();
     }
+
+    get getUsername(){
+        return  this._userName;
+    }
+    set setUsername(yourUserName){
+        this._name = yourUserName;
+    }
+    get getId(){
+        return this._id;
+    }
+    set setId(yourId){
+        this._id = yourId;
+    }
+    get getName(){
+        return this._name;
+    }
+    set setName(yourName){
+        this._name = yourName;
+    }
+    get getBio(){
+        return this._bio;
+    }
+    set setBio(yourBio){
+        this._bio = yourBio;
+    }
+    get getDateCreated(){
+        return this._dataCreated;
+    }
+    get getLastUpdate(){
+        return this._lastUpdate;
+    }
+
 }
 module.exports = User
--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -4,13 +4,40 @@ const User = require('./../../app/models/user')
 describe("Prueba de JEST clase USER twitter", () =>{
 
     test("Pureba 1 :", () =>{
-        const user1 = new User(1,'mikelolp','Miguel Andrade','My bio','12/02/2016','13/04/2022')
+        const user1 = new User(1,'mikelolp','Miguel Andrade','My bio')
         expect(user1._id).toBe(1);
         expect(user1._userName).toBe('mikelolp');
         expect(user1._name).toBe('Miguel Andrade');
         expect(user1._bio).toBe('My bio');
-        expect(user1._dataCreated).toBe('12/02/2016');
-        expect(user1._lastUpdate).toBe('13/04/2022');
+        expect(user1._dataCreated).not.toBeUndefined();
+        expect(user1._dataCreated).not.toBeUndefined();
 
+    })
+
+    test("Pruebas 2: Fechas con tipos fechas: ", () => {
+        const user2 = new User(2,'katygn','Sarai Gonzalez', 'Her bio');
+        expect(user2._dataCreated).not.toBeUndefined();// para que no mande un valor indefinido
+        expect(user2._lastUpdate).not.toBeUndefined();
+        expect(user2._name).toBe('Sarai Gonzalez');
+        expect(user2._userName).toBe('katygn');
+        expect(user2._id).toBe(2);
+        expect(user2._bio).toBe('Her bio');
+    })
+
+    test("Prueba 3: Getters section :", () => {
+        const user3 = new User(3,'wavelol117', 'Eduardo Andrade', 'His bio')
+        expect(user3.getUsername).toBe('wavelol117');
+        expect(user3.getId).toBe(3);
+        expect(user3.getName).toBe('Eduardo Andrade');
+        expect(user3.getBio).toBe('His bio');
+        expect(user3.getDateCreated).not.toBeUndefined();
+        expect(user3.getLastUpdate).not.toBeUndefined();   
+    })
+
+    test("Pruebas 4: Setters section :", () => {
+        const user4 = new User(4, 'Alexcril', 'Alex Lopez','His bio');
+        expect(user4.getName).toBe('Alex Lopez');
+        user4.setName = 'Alex Rio';
+        expect(user4._name).toBe('Alex Rio');
     })
 })
